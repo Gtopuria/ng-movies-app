5b8ddaad9dec74127541f68946f32bf79341d5f4
gitopu <gitopu@ttu.ee>
2018-09-16 13:35:16 +0300

    ensuring state immutability

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,10 +1,11 @@
-import { createStore, compose, StoreEnhancer } from 'redux';
+import { createStore, compose, StoreEnhancer, applyMiddleware } from 'redux';
 import { reducer } from './reducer';
+import freezeState from './freezeState';
 
 declare var window: any;
 
 const devToolsExtension: StoreEnhancer = (window.devToolsExtension)
     ? window.devToolsExtension() : (f) => f;
 
-export const store = createStore(reducer, compose(devToolsExtension) as StoreEnhancer);
+export const store = createStore(reducer, compose(applyMiddleware(freezeState), devToolsExtension) as StoreEnhancer);
 
